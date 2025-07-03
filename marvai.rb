06545cf699a38ae38716c0e85db546ceff39116e
Stephan Schmidt <stephan.schmidt@gmail.com>
2025-07-03 20:00:23 +0200

    Version 0.1.10

--- marvai.rb
+++ marvai.rb
@@ -3,6 +3,7 @@ class Marvai < Formula
   homepage "https://github.com/marvai-dev/marvai"
   url "https://github.com/marvai-dev/marvai/archive/refs/tags/v0.1.10.tar.gz"
   sha256 "b6b1320561c692045f274132e886a1c62d37ad951a8e58cd8a287dc77e15a0cc"
+  version "0.1.10"
   license "MIT"
 
   depends_on "go" => :build
