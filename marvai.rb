58ab35194ce3c1628bc11d5855988ce26695628e
Stephan Schmidt <stephan.schmidt@gmail.com>
2025-07-03 20:32:19 +0200

    Version 0.1.13

--- marvai.rb
+++ marvai.rb
@@ -1,8 +1,8 @@
 class Marvai < Formula
   desc "CLI packaging tool for managing and executing AI prompts in your projects"
   homepage "https://github.com/marvai-dev/marvai"
-  url "https://github.com/marvai-dev/marvai/archive/refs/tags/v0.1.11.tar.gz"
-  sha256 "04057d239038f1b53da4377639dc5a630ebfca6808fa0979fefa242a046f30a0"
+  url "https://github.com/marvai-dev/marvai/archive/refs/tags/v0.1.13.tar.gz"
+  sha256 "c8af6c515206fadf2aa2dc4bf0652bd0b9a71682062079b1524120890b58f84f"
   version "0.1.11"
   license "MIT"
 
